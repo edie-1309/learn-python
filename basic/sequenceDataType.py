e4856a2876f7a50cb024a4233a5dcd82bd182e6c
edie <edinugroho514@gmail.com>
2025-08-11 11:24:44 +0700

    add: sequenceDataType.py and exception.py

---
+++ basic/sequenceDataType.py
@@ -0,0 +1,18 @@
+## List
+# mutable -> setiap elemennya dapat diubah
+# used for store multiple values in variable
+names = ['edie', 'rangga', 'ilham']
+
+# 0 is the first value of items, like array in another programming language
+print(names[0])
+print(names[:2])
+
+## Tuple
+# immutable -> tidak bisa diubah setiap elemennya
+coffee = ("Americano", "Latte")
+print(coffee)
+print(coffee[1])
+
+## Range
+x = range(10)
+print(x)
