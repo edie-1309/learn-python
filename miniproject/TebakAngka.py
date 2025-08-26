ccbad90246ee6811f2ae4f74dc8886f0597efe16
edie <edinugroho514@gmail.com>
2025-08-26 11:29:27 +0700

    update: TebakAngka.py

--- miniproject/TebakAngka.py
+++ miniproject/TebakAngka.py
@@ -6,5 +6,7 @@ user_input_number = input("Masukan tebakan angka: ");
 
 if user_input_number == random_integer:
     print("Benar!")
+elif not isinstance(user_input_number, int):
+    print("Input harus angka!")
 else:
     print("Salah!, angka yang benar adalah: ", random_integer)
