f60049f6eac92051112fbe6e05eb5199d77e78ef
edie <edinugroho514@gmail.com>
2025-08-26 11:25:58 +0700

    add: output_format.py

---
+++ basic/output_format.py
@@ -0,0 +1,10 @@
+name = "Edie"
+age = 30
+height = 5.6
+
+# standard
+print("Hai nama saya", nama)
+
+# with format method
+print(f"Name: {name}, Age: {age}th, Height: {height} feet")
+print(f"Age next year: {age + 1}th")
