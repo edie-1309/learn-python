cd5ced4a640e2fd871f5671d15d8f9a6c0c1c5c3
edie <edinugroho514@gmail.com>
2025-08-26 11:01:20 +0700

    update: conditional.py

--- basic/conditional.py
+++ basic/conditional.py
@@ -17,3 +17,26 @@
 
 # lebih dari atau sama dengan
 # print(5 >= 6)
+
+# conditional statement (operator percabangan), dan ini berkaitan dengan operator pengkondisian
+# if, else, elif
+# nilai_pertama = 3.5
+# nilai_kedua = 3.2
+
+# if nilai_pertama < nilai_kedua:
+#     print("nilai pertama lebih kecil dari nilai kedua")
+# elif nilai_pertama > nilai_kedua:
+#     print("nilai pertama lebih besar dari nilai kedua")
+# else:
+#     print("nilai pertama sama dengan nilai kedua")
+# contoh lain
+username_system = "admin"
+password_system = "admin123"
+
+input_username = input("masukkan username: ")
+input_password = input("masukkan password: ")
+
+if input_username == username_system and input_password == password_system:
+    print("kredesial sesuai, anda berhasil login")
+else:
+    print("kredesial tidak sesuai, silahkan coba lagi")
