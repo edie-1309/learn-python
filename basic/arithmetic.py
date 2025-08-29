bdad60c8d443a6785a89e64472100953cf0bc37a
edie <edinugroho514@gmail.com>
2025-08-29 14:37:22 +0700

    add: arithmetic.py

---
+++ basic/arithmetic.py
@@ -0,0 +1,24 @@
+# operasi aritmatika, digunakan untuk melakukan operasi matematika dasar
+# ( + ) -> penjumlahan
+number1 = 4
+number2 = 2
+resultPenjumlahan = number1 + number2
+
+# ( - ) -> pengurangan
+resultPengurangan = number1 - number2
+
+# ( * ) -> perkalian
+resultPerkalian = number1 * number2
+
+# ( / ) -> pembagian, hasil pembagian akan selalu bertipe data float
+resultPembagianFloat = number1 / number2
+
+#  ( // ) -> pembagian yang bulat, hasil pembagian akan dibulatkan ke bawah
+resultPembagianInteger = number1 // number2
+
+# ( % ) -> modulus, sisa hasil dari pembagian
+resultModulus = number1 % number2
+
+# ( ** ) -> eksponen/pangkat
+resultEksponen = number1 ** number2
+print(resultEksponen)
