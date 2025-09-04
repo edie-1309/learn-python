96d0a854d69f082c6acc2609ce1bfeebcd1fe25a
edie <edinugroho514@gmail.com>
2025-09-04 11:46:52 +0700

    update: listDataType.py

--- basic/listDataType.py
+++ basic/listDataType.py
@@ -19,3 +19,27 @@ print(coffees)
 coffees.pop(2)
 # coffees.pop()  # remove last item
 print(coffees)
+
+# looping
+# for looping
+# 1
+print("## Looping through items")
+for coffee in coffees:
+    print(coffee)
+# 2
+print("## Looping with index")
+for i in range(len(coffees)):
+    print(f"{i+1}. {coffees[i]}")
+# laptop = ['acer', 'asus', 'lenovo']
+# for index in range(len(laptop)):
+# 	print(f"{index+1}. {laptop[index]}")
+# 3
+print("## Shorthand for looping")
+[print(x) for x in coffees]
+
+# while looping
+laptop = ['acer', 'asus', 'lenovo']
+i = 0
+while i < len(laptop):
+    print(f"{i+1}. {laptop[i]}")
+    i = i + 1
