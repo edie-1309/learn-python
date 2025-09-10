a284a81822b15a05af007125cbb289030f8c902c
edie <edinugroho514@gmail.com>
2025-09-10 12:33:10 +0700

    update: main.py

--- main.py
+++ main.py
@@ -8,6 +8,12 @@
 # thisIsMyFavFruit()
 
 # tuple 
-myGadgets = ("Redmi 4x", "Acer Aspire 3 A314", "Earfun Air Life")
-print(len(myGadgets))
+# myGadgets = ("Redmi 4x", "Acer Aspire 3 A314", "Earfun Air Life")
+# print(len(myGadgets))
+# print(myGadgets)
+
+# list
+myGadgets = ["Redmi 4x", "Acer Aspire 3 A314", "Earfun Air Life", "Gamen Titan V"]
+# myGadgets[0] = "Redmi Note 11" # change item in list
+myGadgets[2:4] = ["Xiaomi Buds 3T Pro", "Noir"] # change multiple item in list, from index 2 to 3
 print(myGadgets)
