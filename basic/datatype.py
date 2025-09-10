34253a08e63c70159b860e2593ff93de259bc038
edie <edinugroho514@gmail.com>
2025-09-10 11:59:56 +0700

    update: datatype.py

--- basic/datatype.py
+++ basic/datatype.py
@@ -2,13 +2,13 @@
 string1 = "Hello"
 
 nations = "Germany"
-string2 = f"""
-World War I
-Nations: {nations}
-"""
+string2 = f"""World War I
+Nations: {nations}"""
 
 print(type(string1))
 
 # Float
 number1 = 3.14
 print("Isi dari variabel number1 = " + str(number1) + ", dengan tipe data " + str(type(number1)))
+
+print(string2)
